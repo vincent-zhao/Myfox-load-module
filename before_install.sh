9874e4a15d4612d0ae5cec3c771fd511b323ed01
aleafs <zhangxc83@gmail.com>
2012-03-08 11:00:26 +0800

    Add : install phing in before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
 #
 
+# install apc
 wget http://pecl.php.net/get/APC-3.1.9.tgz && \
 	tar zxvf APC-3.1.9.tgz && \
 	cd APC-3.1.9/ && \
@@ -8,15 +9,14 @@ wget http://pecl.php.net/get/APC-3.1.9.tgz && \
 	./configure && make && make install && \
 	echo "extension=\"apc.so\"" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
+# install igbinary
 wget http://pecl.php.net/get/igbinary-1.1.1.tgz && \
 	tar zxvf igbinary-1.1.1.tgz && cd igbinary-1.1.1 && \
 	phpize && ./configure CFLAGS="-O2 -g" --enable-igbinary && \
 	make && make install && \
 	echo "extension=\"igbinary.so\"" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
 
-pyrus install http://phptal.org/latest.tar.gz
-phpenv rehash
-
-pear channel-discover pear.phing.info
-pear remote-list -c phing
-pear install phing/phing
+# install phing
+mkdir phing && cd phing && \
+	wget http://www.phing.info/get/phing-2.4.9.tgz && \
+	tar zxvf phing-2.4.9.tgz
