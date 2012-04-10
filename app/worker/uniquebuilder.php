599d07355928b09ad72ecc6d8455ac4d062c4a17
aleafs <zhangxc83@gmail.com>
2012-04-10 15:11:29 +0800

    code sync from svn

---
+++ app/worker/uniquebuilder.php
@@ -0,0 +1,404 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+namespace Myfox\App\Worker;
+
+use \Myfox\Lib\Mysql;
+use \Myfox\Lib\Log;
+use \Myfox\App\Setting;
+use \Myfox\App\Model\Server;
+
+class Uniquebuilder extends \Myfox\App\Worker
+{
+
+    /*{{{ variables*/
+    private $mysql;
+
+    private $log;
+
+    private $hosts = array();
+
+    private $splits = array();
+
+    private $prevCheckTime = 0;
+
+    private $checkCount = 3;
+
+    protected $option = array(
+        'sleep'    => 3600000,
+        'interval' => 1000000000000000
+//        'interval' => 259200
+    );
+    /*}}}*/
+
+    /*{{{ __construct()*/
+    /**
+     * 构造函数
+     * @param void
+     */
+    public function __construct($option = array('m' => 'run'))
+    {
+        parent::__construct($option);
+        for($i = 0;$i < 16;$i++){
+            array_push($this->splits,sprintf('route_info_%s',dechex($i)));
+        }
+
+        $this->mysql = \Myfox\Lib\Mysql::instance('default');
+
+        $this->log = new \Myfox\Lib\Log($this->mysql->option('logurl'));
+
+        $hosts = $this->mysql->getAll($this->mysql->query(sprintf(
+            'SELECT host_id,host_name FROM %shost_list',
+            $this->mysql->option('prefix')
+        )));
+
+        foreach($hosts AS $host){
+            $this->hosts[$host['host_id']] = $host['host_name'];
+        }
+    }
+    /*}}}*/
+
+    /*{{{ execute()*/
+    /**
+     * 执行函数
+     * @param {bool} loop 是否循环
+     * @return void
+     */
+    public function execute($loop = true)
+    {
+
+        if($this->option['m'] === 'init'){
+            $this->init_table_unique();
+        }else{
+            $this->set_split_unique();
+        }
+
+    }
+    /*}}}*/
+
+    /*{{{ init_table_unique()*/
+    /**
+     * 初始化table的uniquekey
+     */
+    private function init_table_unique()
+    {
+        $this->log->notice('START INIT',array());
+        $tables  = $this->get_table_list();
+        if(empty($tables)){
+            $this->log->error('GET TABLE LIST FAULT',array());
+        };
+        $length  = count($tables);
+        $counter = 0;
+        
+        foreach($tables AS $table){
+            $counter ++;
+            $uniquekeys = array();
+            $count      = 0;
+            
+            foreach($this->splits AS $split){
+                $routes = $this->get_route_infos($split,$table['table_name']);
+                if(empty($routes)){continue;}
+
+                foreach($routes AS $route){
+                    $begin = time();
+                    if($begin - $route['addtime'] < $this->option['interval']){
+                        if(++$count > $this->checkCount){
+                            break 2;
+                        }
+                        $keys = $this->get_unique_keys($route);
+                        if(empty($keys)){
+                            $count--;
+                            continue;
+                        }
+                        $uniquekeys = array_unique(array_merge($uniquekeys,$keys));
+
+                        $timeuse = time() - $begin;
+                        $this->log->notice('INITING TABLE LIST',array(
+                            'table_name' => $table['table_name'],
+                            'percent'    => ($counter*100 / $length),
+                            'route'      => $route,
+                            'timeUse'    => $timeuse
+                        ));
+                    }
+                }
+            }
+
+            $result = $this->set_table_unique_keys($table['table_name'],$uniquekeys);
+            if(empty($result)){
+                $this->log->error('SET TABLE FAULT',array(
+                    'table'      => $table['table_name'],
+                    'uniquekeys' => $uniquekeys
+                ));
+            }
+        }
+    }
+    /*}}}*/
+
+    /*{{{ set_split_unique()*/
+    /**
+     * 计算并设置分片uniquekey
+     */
+    private function set_split_unique()
+    {
+        $settings = Setting::get('unique_last_check');
+        if(empty($settings)){
+            Setting::set('unique_last_check','2000-01-01');
+        }
+        $this->log->notice('START CHECK',array());
+
+        foreach($this->splits AS $split){
+            $routes = $this->get_route_infos($split);
+            if(empty($routes)){
+                continue;
+            }
+
+            $length  = count($routes);
+            $counter = 0;
+
+            foreach($routes AS $route){
+                $counter ++;
+                $begin = time();
+
+                $keys = $this->get_table_unique_keys($route['table_name']);
+                $uniquekeys = $this->get_unique_keys($route,$keys);
+                if(empty($uniquekeys)){
+                    continue;
+                }
+
+                $timeuse = time() - $begin;
+                $this->log->notice('DEAL NEW SPLIT',array(
+                    'split'   => $split,
+                    'percent' => ($counter*100/$length),
+                    'route'   => $route,
+                    'timeUse' => $timeuse
+                ));
+
+                $result = $this->set_split_unique_keys($split,$route,$uniquekeys);
+                if(empty($result)){
+                    $this->log->error('SET SPLIT FAULT',array(
+                        'split'      => $split,
+                        'route'      => $route,
+                        'uniquekeys' => $uniquekeys
+                    ));
+                }
+            }
+        }
+        Setting::set('unique_last_check',date('Y-m-d H:i:s', time()));
+    }
+    /*}}}*/
+
+    /*{{{ interval()*/
+    /**
+     * 运行execute运行间隔时间
+     * @param void
+     * @return {int} 间隔时间
+     */
+    public function interval()
+    {
+        return $this->option['sleep'];
+    }
+    /*}}}*/
+
+    /*{{{ get_route_infos()*/
+    /**
+     * 获取某条分片的信息
+     * @param {String} split 路由分片表名
+     * @param {String} table 某张具体表的路由信息
+     * @return {Array} 此路由分片表中所有的分片信息
+     */
+    private function get_route_infos($split,$table = '')
+    {
+        $sql = '';
+        if(empty($table)){
+            $sql = sprintf(
+                'SELECT autokid,table_name,real_table,hosts_list FROM %s%s 
+                WHERE route_flag >= %s AND route_flag < %s AND modtime >= %s',
+                $this->mysql->escape($this->mysql->option('prefix')),
+                $this->mysql->escape($split),'300','400',
+                strtotime(Setting::get('unique_last_check'))
+            );
+        }else{
+            $sql = sprintf(
+                "SELECT autokid,addtime,real_table,hosts_list FROM %s%s 
+                WHERE table_name = '%s' AND route_flag >= %s AND route_flag < %s",
+                $this->mysql->escape($this->mysql->option('prefix')),
+                $this->mysql->escape($split),
+                $this->mysql->escape($table),'300','400'
+            );
+        }
+        return $this->mysql->getAll($this->mysql->query($sql));
+    }
+    /*}}}*/
+
+    /*{{{ get_table_list()*/
+    /**
+     * 获得所有表名
+     * @param void
+     * @return {Array|false} 查询正常返回表名数组，不正常返回false
+     */
+    private function get_table_list(){
+        $result = $this->mysql->getAll($this->mysql->query(sprintf(
+            'SELECT table_name FROM %stable_list',
+            $this->mysql->escape($this->mysql->option('prefix'))
+        )));
+        return $result;
+    }
+    /*}}}*/
+
+    /*{{{ get_unique_keys()*/
+    /**
+     * 获取某个分片表的unique key
+     * @param {Array} route 某路由信息
+     * @return {Array} unique key数组
+     */
+    private function get_unique_keys($route,$keys = array())
+    {
+        $hosts = preg_split('/,/',$route['hosts_list'],-1,PREG_SPLIT_NO_EMPTY);
+        $poped = array_pop($hosts);
+        if(substr($poped,-1) === '$'){
+            array_push($hosts,substr($poped,0,strlen($poped)-1));
+        }
+
+        $mysql;
+        $host = (int)array_pop($hosts);
+        try{
+            $mysql  = Server::instance($this->hosts[$host])->getlink();
+        }catch(\Exception $e){
+            $this->log->error('GET SERVER FAULT',array(
+                'host' => $host
+            ));
+            return false;
+        }
+
+        $sql    = 'SELECT %s FROM '.$this->mysql->escape($route['real_table']);
+        $result = array();
+
+        if(empty($keys)){
+            $columns = $mysql->getAll($mysql->query(sprintf(
+                'DESCRIBE %s',
+                $this->mysql->escape($route['real_table'])
+            )));
+            if(empty($columns)){
+                return false;
+            }
+
+            $part = '';
+            foreach($columns AS $column){
+                if(!preg_match('/(int\([1]?[0-9]\))|(varchar\([1-6]?[0-9]\))/i',$column['Type'])){
+                    continue;
+                }
+
+                $part = $part.sprintf(
+                    'COUNT(DISTINCT(%s)) AS %s,',
+                    $this->mysql->escape($column['Field']),
+                    $this->mysql->escape($column['Field'])
+                );
+            }
+            $part = $part.'COUNT(*) AS total';
+
+            $get  = $mysql->getAll($mysql->query(sprintf($sql,$part)));
+
+            foreach($columns AS $column){
+                if(empty($get[0][$column['Field']])){
+                    continue;
+                }
+
+                if($get[0][$column['Field']] >= $get[0]['total']*0.8){
+                    array_push($result,$column['Field']);
+                }
+            }
+
+        }else{
+            $part = '';
+            foreach($keys AS $key){
+                $part = $part.sprintf(
+                    'COUNT(DISTINCT(%s)) AS %s,',
+                    $this->mysql->escape($key),
+                    $this->mysql->escape($key)
+                );
+            }
+            $part = $part.'COUNT(*) AS total';
+
+            $get  = $mysql->getAll($mysql->query(sprintf($sql,$part)));
+
+            foreach($keys AS $key){
+                if($get[0][$key] >= $get[0]['total']*0.8){
+                    array_push($result,$key);
+                }
+            }
+        }
+
+        return $result;
+    }
+    /*}}}*/
+
+    /*{{{ get_table_unique_keys()*/
+    /**
+     * 获得某张表的uniquekeys
+     * @param {String} table 表名
+     * @return {Array|bool} 正常返回表uniquekeys，数据库数据查询失败返回false
+     */
+    private function get_table_unique_keys($table){
+        $result = $this->mysql->getAll($this->mysql->query(sprintf(
+            "SELECT unique_key FROM %stable_list WHERE table_name = '%s'",
+            $this->mysql->escape($this->mysql->option('prefix')),
+            $this->mysql->escape($table)
+        )));
+        if(empty($result)){return $result;}
+
+        $keystring    = $result[0]['unique_key'];
+        $uniquekeys   = array();
+        if(strlen($keystring) !== 0){
+            $uniquekeys = preg_split('/;/',$keystring,-1,PREG_SPLIT_NO_EMPTY);
+            array_push($uniquekeys,(substr(array_pop($uniquekeys),0,-1)));
+        }
+
+        return $uniquekeys;
+    }
+    /*}}}*/
+
+    /*{{{ set_split_unique_keys()*/
+    /**
+     * 设置每个分片路由的uniquekey值
+     * @param {String} split 分片表名
+     * @param {Array} route分片路由信息
+     * @param {Array} uniquekeys unique字段们
+     * @return void
+     */
+    private function set_split_unique_keys($split,$route,$uniquekeys)
+    {
+        array_push($uniquekeys,(array_pop($uniquekeys).'$'));
+        $keystring = implode(';',$uniquekeys);
+        return $this->mysql->query(sprintf(
+            "UPDATE %s%s SET unique_key = '%s' WHERE autokid = %s",
+            $this->mysql->escape($this->mysql->option('prefix')),
+            $this->mysql->escape($split),
+            $this->mysql->escape($keystring),
+            $this->mysql->escape($route['autokid'])
+        ));
+    }
+    /*}}}*/
+
+    /*{{{ set_table_unique_keys()*/
+    /**
+     * 设置表的uniqueKey值
+     * @param {String} tableName 表名
+     * @param {Array} uniqueKeys uniqueKey数组
+     * @return void
+     */
+    private function set_table_unique_keys($tableName,$uniqueKeys)
+    {
+        if(empty($uniqueKeys)){return;}
+        array_push($uniqueKeys,(array_pop($uniqueKeys).'$'));
+        $uniqueKeyStr = implode(';',$uniqueKeys);
+        $sql = sprintf(
+            "UPDATE %stable_list SET unique_key = '%s' WHERE table_name = '%s'",
+            $this->mysql->escape($this->mysql->option('prefix')),
+            $this->mysql->escape($uniqueKeyStr),
+            $this->mysql->escape($tableName)
+        );
+        return $this->mysql->query($sql);
+    }
+    /*}}}*/
+
+}
---
+++ test/unit/UniquebuilderTest.php
@@ -0,0 +1,312 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
+
+use \Myfox\App\Worker\Uniquebuilder;
+use \Myfox\Lib\Config;
+use \Myfox\Lib\Mysql;
+use \Myfox\App\Setting;
+use \Myfox\App\Model\Server;
+
+require_once(__DIR__ . '/../../lib/TestShell.php');
+
+class UniquebuilderTest extends \Myfox\Lib\TestShell{
+
+	private $mysql;
+
+	private $infos = array();
+
+	/*{{{ 表备份*/
+	private function table_bak($tableName){
+
+		$this->mysql->query(sprintf(
+			'CREATE TABLE %s%s LIKE %s%s',
+			$this->mysql->option('prefix'),
+			$tableName.'_bak',
+			$this->mysql->option('prefix'),
+			$tableName
+		));
+
+		$this->mysql->query(sprintf(
+			'INSERT INTO %s%s SELECT * FROM %s%s',
+			$this->mysql->option('prefix'),
+			$tableName.'_bak',
+			$this->mysql->option('prefix'),
+			$tableName
+		));
+
+		$this->mysql->query(sprintf(
+			'DELETE FROM %s%s',
+			$this->mysql->option('prefix'),
+			$tableName
+		));
+
+	}
+	/*}}}*/
+
+	/*{{{ 表恢复*/
+	private function table_recover($tableName){
+
+		$this->mysql->query(sprintf(
+			'DELETE FROM %s%s',
+			$this->mysql->option('prefix'),
+			$tableName
+		));
+
+		$this->mysql->query(sprintf(
+			'INSERT INTO %s%s SELECT * FROM %s%s',
+			$this->mysql->option('prefix'),
+			$tableName,
+			$this->mysql->option('prefix'),
+			$tableName.'_bak'
+		));
+
+		$this->mysql->query(sprintf(
+			'DROP TABLE %s%s',
+			$this->mysql->option('prefix'),
+			$tableName.'_bak'
+		));
+
+	}
+	/*}}}*/
+
+	/*{{{ setUp() */
+	/**
+	 * 测试表及信息创建
+	 */
+	protected function setUp()
+	{
+
+		parent::setUp();
+		\Myfox\Lib\Mysql::removeAllNames();
+
+		\Myfox\Lib\Mysql::register('default',__DIR__.'/ini/mysql.ini');
+		$this->mysql = \Myfox\Lib\Mysql::instance('default');
+
+		$config = new \Myfox\Lib\Config(__DIR__.'/ini/mysql.ini');
+		foreach($config->get('master') as $host){
+			$urlRes = parse_url($host);
+			$this->infos['host'] = $urlRes['host'];
+			$this->infos['port'] = array_key_exists('port',$urlRes) ? $urlRes['port'] : 3306;
+			$this->infos['user'] = array_key_exists('user',$urlRes) ? $urlRes['user'] : 'root';
+			$this->infos['pass'] = array_key_exists('pass',$urlRes) ? $urlRes['pass'] : '';
+		}
+
+        self::cleanTable('default', 'route_info');
+		$this->table_bak('table_list');
+		$this->table_bak('host_list');
+		$this->table_bak('settings');
+
+		//插入测试分片表1
+		$this->mysql->query('CREATE TABLE `test_table_real1` (
+			`id` int(10) unsigned NOT NULL auto_increment,
+			`key1` varchar(64) NOT NULL default "",
+			`key2` int(10) NOT NULL default 0,
+			PRIMARY KEY (`id`)
+		)');
+
+		//插入测试分片表2
+		$this->mysql->query('CREATE TABLE `test_table_real2` (
+			`id` int(10) unsigned NOT NULL auto_increment,
+			`key1` varchar(64) NOT NULL default "",
+			`key2` int(10) NOT NULL default 0,
+			PRIMARY KEY (`id`)
+		)');
+
+		//插入测试分片表3
+		$this->mysql->query('CREATE TABLE `test_table_real3` (
+			`id` int(10) unsigned NOT NULL auto_increment,
+			`key1` varchar(64) NOT NULL default "",
+			`key2` int(10) NOT NULL default 0,
+			PRIMARY KEY (`id`)
+		)');
+
+	}
+	/*}}}*/
+
+	/*{{{ tearDown()*/
+	/**
+	 * 删除用的表
+	 */
+	protected function tearDown()
+	{
+
+		$this->table_recover('table_list');
+		$this->table_recover('host_list');
+		$this->table_recover('settings');
+
+		$this->mysql->query("DROP TABLE IF EXISTS test_table_real1");
+		$this->mysql->query("DROP TABLE IF EXISTS test_table_real2");
+		$this->mysql->query("DROP TABLE IF EXISTS test_table_real3");
+
+	}
+	/*}}}*/
+
+	/*{{{ test_init_works_fine()*/
+	public function test_init_works_fine()
+	{
+		//向dev_table_list测试表中插入数据
+		$this->mysql->query(sprintf(
+			"insert into %stable_list (table_name) values ('testTable')",
+			$this->mysql->option("prefix")
+		));
+
+		//插入具体测试路由数据
+		$this->mysql->query(sprintf(
+			"insert into %sroute_info (table_name,addtime,modtime,real_table,hosts_list,route_flag) values ('testTable',%s,%s,'meta_myfox_config.test_table_real1','99$',300)",
+			$this->mysql->option("prefix"),time(),time()
+		));
+		$this->mysql->query(sprintf(
+			"insert into %sroute_info (table_name,addtime,modtime,real_table,hosts_list,route_flag) values ('testTable',%s,%s,'meta_myfox_config.test_table_real2','99$',300)",
+			$this->mysql->option("prefix"),time(),time()
+		));
+
+		//插入host测试信息
+		$this->mysql->query(sprintf(
+			"insert into %shost_list (host_id,host_name,conn_host,conn_port,read_user,read_pass) values (99,'testHost','%s',%s,'%s','%s')",
+			$this->mysql->option("prefix"),$this->infos["host"],$this->infos["port"],$this->infos["user"],$this->infos["pass"]
+		));
+
+		$this->mysql->query("insert into test_table_real1 (key1,key2) values ('a',2)");
+		$this->mysql->query("insert into test_table_real1 (key1,key2) values ('b',2)");
+
+		$this->mysql->query("insert into test_table_real2 (key1,key2) values ('a',2)");
+		$this->mysql->query("insert into test_table_real2 (key1,key2) values ('b',1)");
+
+		$uniqueBuilder = new \Myfox\App\Worker\Uniquebuilder(array(
+			'm' => 'init',
+			'checkInterval' => 1000000000000 
+		));
+		$uniqueBuilder->execute();
+
+		//testTable有三个可能唯一字段
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select unique_key from %stable_list where table_name='testTable'",
+			$this->mysql->option("prefix")
+		)));
+		$this->assertEquals(array(
+			'unique_key' => 'id;key1;key2$'
+		),$res[0]);
+
+	}
+	/*}}}*/
+
+	/*{{{ test_set_split_works_fine()*/
+	public function test_set_split_works_fine()
+	{
+
+		$this->test_init_works_fine();
+
+		$this->mysql->query(sprintf(
+			"insert into %ssettings (cfgname,cfgvalue,addtime,modtime) values ('table_last_sync','%s','%s','%s'),('hosts_last_sync','%s','%s','%s'),('route_last_sync','%s','%s','%s')",
+			$this->mysql->option('prefix'),'2012-03-12 18:02:27','2012-03-12 18:02:30','2012-03-12 18:02:30','2012-03-12 18:02:30','2012-03-12 18:02:30','2012-03-12 18:02:30','2012-03-13 10:36:11','2012-03-13 09:56:27','2012-03-13 10:36:11'
+		));
+
+		$originalModtime = Setting::get('unique_last_check');
+
+		$uniqueBuilder = new \Myfox\App\Worker\Uniquebuilder();
+		$uniqueBuilder->execute();
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select unique_key from %sroute_info where table_name = 'testTable'",
+			$this->mysql->option("prefix")
+		)));
+		$this->assertEquals(array(
+			'unique_key' => 'id;key1$'
+		),$res[0]);
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select unique_key from %sroute_info where table_name = 'testTable'",
+			$this->mysql->option("prefix")
+		)));
+		$this->assertEquals(array(
+			'unique_key' => 'id;key1;key2$'
+		),$res[0]);
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select cfgvalue,modtime from %ssettings where cfgname = 'unique_last_check'",
+			$this->mysql->option("prefix")
+		)));
+
+		$this->assertFalse($originalModtime === $res[0]['modtime']);
+
+	}
+	/*}}}*/
+
+	/*{{{ test_table_modified_works_fine()*/
+	public function test_table_modified_works_fine()
+	{
+		$this->test_set_split_works_fine();
+
+		sleep(3);
+
+		$this->mysql->query(sprintf(
+			"UPDATE test_table_real1 SET key2 = 3 WHERE key1 = 'b'"
+		));
+
+		$this->mysql->query(sprintf(
+			"UPDATE %sroute_info SET modtime = %s WHERE table_name = 'testTable'",
+			$this->mysql->option('prefix'),time()
+		));
+
+		$originalModtime = Setting::get('unique_last_check');
+
+		$uniqueBuilder = new \Myfox\App\Worker\Uniquebuilder();
+		$uniqueBuilder->execute();
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select unique_key from %sroute_info where table_name='testTable'",
+			$this->mysql->option("prefix")
+		)));
+		$this->assertEquals(array(
+			'unique_key' => 'id;key1;key2$'
+		),$res[0]);
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select cfgvalue,modtime from %ssettings where cfgname = 'unique_last_check'",
+			$this->mysql->option("prefix")
+		)));
+
+		$this->assertFalse($originalModtime === $res[0]['modtime']);
+
+	}
+	/*}}}*/
+
+	/*{{{ test_add_new_split_works_fine()*/
+	public function test_add_new_split_works_fine()
+	{
+		$this->test_set_split_works_fine();
+
+		sleep(3);
+
+		$this->mysql->query(sprintf(
+			"insert into %sroute_info (table_name,addtime,modtime,real_table,hosts_list,route_flag) values ('testTable',%s,%s,'meta_myfox_config.test_table_real3','99$',300)",
+			$this->mysql->option("prefix"),time(),time()
+		));
+
+		$this->mysql->query("insert into test_table_real3 (key1,key2) values ('a',2)");
+		$this->mysql->query("insert into test_table_real3 (key1,key2) values ('b',5)");
+
+		$originalModtime = Setting::get('unique_last_check');
+
+		$uniqueBuilder = new \Myfox\App\Worker\Uniquebuilder();
+		$uniqueBuilder->execute();
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select unique_key from %sroute_info where table_name = 'testTable'",
+			$this->mysql->option("prefix")
+		)));
+		$this->assertEquals(array(
+			'unique_key' => 'id;key1;key2$'
+		),$res[0]);
+
+		$res = $this->mysql->getAll($this->mysql->query(sprintf(
+			"select cfgvalue,modtime from %ssettings where cfgname = 'unique_last_check'",
+			$this->mysql->option("prefix")
+		)));
+
+		$this->assertFalse($originalModtime === $res[0]['modtime']);
+
+	}
+	/*}}}*/
+
+}
